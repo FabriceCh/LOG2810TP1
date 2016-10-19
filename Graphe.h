cf81432022565ac934401a11affde93ca119ff36
FabriceCh <fabrice.charbonneau@hotmail.com>
2016-10-19 12:30:57 -0400

    Plus de fonctions et idee de base pour extraction

--- Graphe.h
+++ Graphe.h
@@ -16,8 +16,17 @@ class Graphe
 		Graphe();
 		//constructeur par paramètre
 		Graphe(int ess, int essMax, int ele, int eleMax, list<Arc> listeArc, list<Sommet> listeSommet);
-		//
-
+		//Sets, Gets
+		int GetEssence();
+		void SetEssence(int e);
+		int GetEssenceMax();
+		void SetEssenceMax(int e);
+		int GetElectricite();
+		void SetElectricite(int e);
+		int GetElectriciteMax();
+		void SetElectriciteMax(int e);
+		void AjouterArc(Arc arc);
+		void EnleverArc(Arc arc);
 private:
 		int essenceMax_;
 		int essence_;
--- Sommet.h
+++ Sommet.h
@@ -8,8 +8,20 @@ class Sommet
 {
 	public:
 		Sommet();
-		Sommet(string type, list<Arc> arcs);
+		Sommet(string type, list<Arc> arcs, list<Arc> arcsA);
 	private:
 		string type_;
 		list<Arc> arcs_;
+		list<Arc> arcsAccessibles_;
 };
+/*
+EXTRACTION GRAPH 
+parcour(Sommet sommet)
+{ //graph2 est le nouveau sous-graph
+	for each arc in sommet
+		if arc.distance < essence + electricite && qteEssence > qteEssenceMax
+			ajouter arc in graph2
+			ajoute arc.sommetb in graph2 si 
+			parcour(sommetb)
+}
+*/
